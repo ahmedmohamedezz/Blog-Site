88fbf0a3fd29d3a85b6a5e969dc4667044440a65
ahmedmohamedezz <ahmedmohamedezz111@gmail.com>
2025-12-22 21:03:16 +0200

    add search-text functionality using trigram similarity

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 from django.db.models import Count
@@ -147,9 +148,27 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.published.annotate(
-                search=SearchVector("title", "body")
-            ).filter(search=query)
+
+            # approach 1: using search vectors + weights for ranking
+            # search_vector = SearchVector("title", weight="A") + SearchVector(
+            #     "body", weight="B"
+            # )
+            # search_query = SearchQuery(query)
+            # results = (
+            #     Post.published.annotate(
+            #         search=SearchVector("title", "body"),
+            #         rank=SearchRank(search_vector, search_query),
+            #     )
+            #     .filter(rank__gte=0.3)
+            #     .order_by("-rank")
+            # )
+
+            # approach 2: using tri-gram extension in postgres
+            results = (
+                Post.published.annotate(similarity=TrigramSimilarity("title", query))
+                .filter(similarity__gt=0.1)
+                .order_by("-similarity")
+            )
 
     return render(
         request,
